4276aedc6cb74629a70fe9634f1a5ecc663ac238
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2022-09-12 18:39:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Flutter 3.0 for the first time
+
+[Following this YouTube video](https://www.youtube.com/watch?v=71AsYo2q_0Y)
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0xFF687daf);
+
+class Styles {
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = const Color(0xFFeeedf2);
+  static Color orangeColor = const Color(0xFF526799);
+
+  static TextStyle textStyle =
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
+  static TextStyle headLineStyle =
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle2 =
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  static TextStyle headLineStyle3 = TextStyle(
+      fontSize: 16, fontWeight: FontWeight.w500, color: Colors.grey.shade400);
+  static TextStyle headlineStyle4 = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.w500, color: Colors.grey.shade400);
+}
